a0d9703e5c45ebd3058c43161b22221917866db5
Kaninchentu <106641117+Kaninchentu@users.noreply.github.com>
2023-05-17 14:34:10 +0800

    Update red.py

--- red.py
+++ red.py
@@ -1,4 +1,10 @@
+import cv2
+import numpy as np
 import time
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
 class PIDController:
     def __init__(self, Kp, Ki, Kd):
@@ -14,37 +20,83 @@ class PIDController:
         self.last_error = error
         return self.Kp * error + self.Ki * self.error_sum + self.Kd * derivative
 
+class MotorDriver:
+    I2C_ADDR = 0x34
+    MOTOR_TYPE_ADDR = 0x14
+    MOTOR_ENCODER_POLARITY_ADDR = 0x15
+    MOTOR_FIXED_PWM_ADDR = 0x1F
+    MOTOR_FIXED_SPEED_ADDR = 0x33
+    MOTOR_ENCODER_TOTAL_ADDR = 0x3C
+    
+    # MOTOR_TYPE_WITHOUT_ENCODER = 0
+    MOTOR_TYPE_TT = 1
+    # MOTOR_TYPE_N20 = 2
+    # MOTOR_TYPE_JGB37_520_12V_110RPM = 3
 
-# 以下是假设的电机控制代码
-class Motor:
-    def set_speed(self, speed):
-        pass  # 在这里添加你的电机控制代码
+
+    def __init__(self, bus):
+        self.dev = I2CDevice(bus, self.I2C_ADDR)
+        self.set_speed_all([0, 0, 0, 0])
 
 
-def get_line_position():
-    pass  # 在这里添加你的图像处理代码，返回线的位置（从-1到1）
+    def set_motor_type(self, value):
+        self.dev.write_byte(self.MOTOR_TYPE_ADDR, value)
 
 
-# 初始化
-motor_left = Motor()
-motor_right = Motor()
-pid = PIDController(0.5, 0.1, 0.2)  # 你需要调整这些参数以适应你的设备
-base_speed = 0.5  # 基本速度
-max_speed = 1.0  # 最大速度
+    def set_encoder_polarity(self, value):
+        self.dev.write_byte(self.MOTOR_ENCODER_POLARITY_ADDR, value)
 
-# 主循环
+
+    def set_speed(self, idx, speed):
+        self.dev.write_byte(self.MOTOR_FIXED_SPEED_ADDR + idx - 1, speed)
+
+
+    def set_speed_all(self, speed_list):
+        self.dev.write_bytes(self.MOTOR_FIXED_SPEED_ADDR, speed_list)
+
+
+
+def detect_red_line(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    return mask
+
+def get_line_position(mask):
+    moments = cv2.moments(mask)
+    if moments['m00'] != 0:
+        cX = moments['m10'] / moments['m00']
+        position = (cX - mask.shape[1] // 2) / (mask.shape[1] // 2)
+        return position
+    else:
+        return None
+
+motor = MotorDriver()
+pid = PIDController(0.5, 0.1, 0.2)
+base_speed = 10
+max_speed = 10
+
 last_time = time.time()
-while True:
-    line_position = get_line_position()
-    current_time = time.time()
-    dt = current_time - last_time
-    last_time = current_time
+while cap.isOpened():
+    ret, frame = cap.read()
+    mask = detect_red_line(frame)
+    line_position = get_line_position(mask)
 
-    # PID 控制
-    speed_correction = pid.control(line_position, dt)
+    if line_position is not None:
+        current_time = time.time()
+        dt = current_time - last_time
+        last_time = current_time
+        speed_correction = pid.control(line_position, dt)
 
-    # 控制电机速度
-    speed_left = min(max(base_speed - speed_correction, -max_speed), max_speed)
-    speed_right = min(max(base_speed + speed_correction, -max_speed), max_speed)
-    motor_left.set_speed(speed_left)
-    motor_right.set_speed(speed_right)
+        speed_left = min(max(base_speed - speed_correction, -max_speed), max_speed)
+        speed_right = min(max(base_speed + speed_correction, -max_speed), max_speed)
+        motor.set_speed(speed_left, speed_right)
+
+    cv2.imshow('Red line', mask)
+    key = cv2.waitKey(1)
+    if key & 0x00FF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
